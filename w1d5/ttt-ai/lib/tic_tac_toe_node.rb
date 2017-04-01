c7e11c01e9aa74ee86fefff9a2c904b20760f25f
Grey Keith <r.grey.keith@gmail.com>
2017-03-31 17:38:39 -0700

    Start to create TTTnode

---
+++ w1d5/ttt-ai/lib/tic_tac_toe_node.rb
@@ -0,0 +1,31 @@
+require_relative 'tic_tac_toe'
+
+class TicTacToeNode
+  def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
+  end
+
+  def losing_node?(evaluator)
+  end
+
+  def winning_node?(evaluator)
+  end
+
+  # This method generates an array of all moves that can be made after
+  # the current move.
+  def children
+    (0..2).each do |row|
+      (0..2).each do |col|
+        pos = [row,col]
+        if @board[pos].empty?
+          TicTacToeNode.new(new_board(pos),
+  end
+
+  def new_board(pos)
+    new_board = @board.dup
+    new_board[pos] = @next_mover_mark
+    new_board
+  end
+end
